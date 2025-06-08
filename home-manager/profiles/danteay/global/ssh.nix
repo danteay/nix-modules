1abc6fbfcbddf8edcaf348b313423e5780c0b6ec
Eduardo Aguilar <dante.aguilar41@gmail.com>
2025-06-07 19:35:54 -0600

    chore: save

--- home-manager/profiles/danteay/global/ssh.nix
+++ home-manager/profiles/danteay/global/ssh.nix
@@ -2,7 +2,7 @@
 {
   home.file = {
     ".ssh/config".source = ../../../../dotfiles/danteay/ssh/config;
-    ".ssh/github".source = ../../../../dotfiles/danteay/ssh/github;
-    ".ssh/github.pub".source = ../../../../dotfiles/danteay/ssh/github.pub;
+#    ".ssh/github".source = ../../../../dotfiles/danteay/ssh/github;
+#    ".ssh/github.pub".source = ../../../../dotfiles/danteay/ssh/github.pub;
   };
 }
